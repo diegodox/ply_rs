c4afa2d32e1e2d063c44efa939b233f95a50a48b
diegodox <android.mxdiego9@gmail.com>
2022-12-21 05:25:03 +0900

    split payload

--- src/lib.rs
+++ src/lib.rs
@@ -260,37 +260,3 @@ impl PropertyList {
     }
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct Payload(Vec<PLYValue>);
-
-impl IntoIterator for Payload {
-    type Item = PLYValue;
-
-    type IntoIter = std::vec::IntoIter<Self::Item>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.into_iter()
-    }
-}
-
-impl Payload {
-    pub fn push_value(&mut self, v: PLYValue) {
-        self.0.push(v);
-    }
-}
-impl From<Vec<PLYValue>> for Payload {
-    fn from(v: Vec<PLYValue>) -> Self {
-        Self(v)
-    }
-}
-impl Deref for Payload {
-    type Target = [PLYValue];
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-impl DerefMut for Payload {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
